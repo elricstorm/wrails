50968f7f06f972cd0728ceed123052303cf76aae
Ealric <jdezenzio@gmail.com>
2013-12-23 13:14:34 -0500

    testing

---
+++ publish_wrails.ps1
@@ -0,0 +1,21 @@
+﻿# =============================================================================================
+# =============================================================================================
+
+# START OF SCRIPT
+
+# Clear and stop other PowerShell Instances
+clear
+echo "__________________________________________________________________"
+echo "===> Stopping other PowerShell Instances"
+Get-Process Powershell  | Where-Object { $_.ID -ne $pid } | Stop-Process
+
+# Push to Github.io.wiki
+echo "__________________________________________________________________"
+echo "===> COMMITTING to Github"
+echo "_____________________USER INPUT REQUIRED!_________________________"
+$commit = Read-Host 'Please type your commit message...'
+git add -A
+git commit -m $commit
+git push origin master
+echo "... FINISHED Pushing to Github";
+# END OF SCRIPT
